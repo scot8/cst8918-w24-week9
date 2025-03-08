894ab6bf8f8440bd0c2f02d44668b3c3b8c15aab
Scot8 <rohansco18@gmail.com>
2025-03-08 00:06:03 -0500

    Added GitHub Actions pipeline for Terraform validation

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_resource_group" "rg" {
   location = "canadacentral"
 }
 
-# Define the storage account
+# Define the storage account, 
 resource "azurerm_storage_account" "storage" {
   name                     = "examplestoracc7890"
   resource_group_name      = azurerm_resource_group.rg.name
@@ -35,3 +35,14 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "examplestoracc7890"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+
+
