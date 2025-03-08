7c164f2d6322d88c811da75f35758ec0d383dff3
Scot8 <rohansco18@gmail.com>
2025-03-07 20:08:23 -0500

    PRE-COMMITS DONE

---
+++ main.tf
@@ -0,0 +1,37 @@
+# Configure the Terraform runtime requirements.
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    # Azure Resource Manager provider and version
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.3.3"
+    }
+  }
+}
+
+# Define providers and their config params
+provider "azurerm" {
+  # Leave the features block empty to accept all defaults
+  features {}
+}
+
+# Define the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "example-rg"
+  location = "westus3"
+}
+
+# Define the storage account
+resource "azurerm_storage_account" "storage" {
+  name                     = "examplestoracc7890"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
